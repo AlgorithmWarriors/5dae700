321fbb0a2c611a8d7862ad31f674240962119ba0
ChUngHEe0227 <rogo0227@naver.com>
2020-06-12 21:56:03 +0900

    스킬 트리 코드 테스트 코드 전부 통과함 새로운 테스트 코드 필요

---
+++ src/test/java/Chunghee/SkillTreeTest.java
@@ -0,0 +1,19 @@
+package Chunghee;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+public class SkillTreeTest {
+    @Test
+    public void 스킬트리테스트(){
+        SkillTree skillTree = new SkillTree();
+        String[] myString1 = {"AB", "BA","CDEAFG","DCNSWUEO"};
+        String[] myString2 = {"AB", "BAC"};
+        String[] myString3 = {"AB", "BACD"};
+        assertThat(skillTree.solution("Q",myString1),is(4));
+        assertThat(skillTree.solution("A",myString2),is(2));
+        assertThat(skillTree.solution("b",myString3),is(2));
+    }
+
+}
