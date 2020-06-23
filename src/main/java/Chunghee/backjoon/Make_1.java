085c0c367ec74e594aff8e0299758d24f030dfe6
ChUngHEe0227 <rogo0227@naver.com>
2020-06-23 19:43:20 +0900

    백준 1463

---
+++ src/main/java/Chunghee/backjoon/Make_1.java
@@ -0,0 +1,15 @@
+package Chunghee.backjoon;
+
+
+public class Make_1 {
+    public int solution(int numbers) {
+        int dp[] = new int[numbers+1];
+        dp[0]= dp[1] = 0;
+        for (int i = 2; i <= numbers; i++) {
+            dp[i] = dp[i - 1] + 1;
+            if (i % 2 == 0) dp[i] = Math.min(dp[i], dp[i / 2] + 1);
+            if (i % 3 == 0) dp[i] = Math.min(dp[i], dp[i / 3] + 1);
+        }
+        return dp[numbers];
+    }
+}
