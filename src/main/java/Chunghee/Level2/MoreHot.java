bbd31fc072ade0f8a69943ff31e8bb9f867db136
ChUngHEe0227 <rogo0227@naver.com>
2020-07-30 19:04:50 +0900

    더맵게 코드 시간 재는 테스트 코드 추가요망

--- src/main/java/Chunghee/Level2/MoreHot.java
+++ src/main/java/Chunghee/Level2/MoreHot.java
@@ -1,20 +1,23 @@
 package Chunghee.Level2;
 
-import java.util.Arrays;
+import java.util.PriorityQueue;
+import java.util.Queue;
 
 public class MoreHot {
     public int solution(int[] scoville, int K) {
         int answer = 0;
-        Arrays.sort(scoville);
-        for(int i=0;i<scoville.length-1;i++){
-            if(scoville[i]<K) {
-                scoville[i + 1] = scoville[i] + scoville[i + 1] * 2;
-                scoville[i]=-1;
-                answer++;
-            }
-            Arrays.sort(scoville);
+        Queue<Integer> myQueue = new PriorityQueue<>();
+        for(int task : scoville){
+            myQueue.offer(task);
         }
-        if(scoville[scoville.length-1]<K)return -1;
+        while (myQueue.peek()<K){
+            if(myQueue.size()<=1)return -1;
+
+            int first=myQueue.poll();
+            int Second=myQueue.poll();
+            myQueue.offer(first+Second*2);
+            answer++;
+        }
         return answer;
     }
 }
