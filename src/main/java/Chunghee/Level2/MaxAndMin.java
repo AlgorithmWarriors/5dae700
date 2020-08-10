389f667e9943a62af358e7238df13dcf38aac949
ChUngHEe0227 <rogo0227@naver.com>
2020-08-10 14:22:34 +0900

    최댓값과 최솟값 (parseInt 이용한 리펙토링)

--- src/main/java/Chunghee/Level2/MaxAndMin.java
+++ src/main/java/Chunghee/Level2/MaxAndMin.java
@@ -1,42 +1,17 @@
 package Chunghee.Level2;
 
 public class MaxAndMin {
-    static char[] myArray;
     public String solution(String s) {
         String answer = "";
-        myArray = s.toCharArray();
-        int Max=findNumber(0);
-        int Min=findNumber(0);
-
-        for(int i=0;i<myArray.length;i++){
-            Max = Math.max(Max,findNumber(i));
-            Min = Math.min(Min,findNumber(i));
-            i+=IndexCalcul(i);
+        String[] myString=s.split(" ");
+        int Max,Min,Number;
+        Max=Min=Integer.parseInt(myString[0]);
+        for(int i=1;i<myString.length;i++){
+            Number = Integer.parseInt(myString[i]);
+            Max = Math.max(Max,Number);
+            Min = Math.min(Min,Number);
         }
         return answer+Min+" "+Max;
-    }
-
-    private int IndexCalcul(int StartIndex) {
-        int index=0;
-        for(int i=StartIndex;i<myArray.length;i++){
-            if(myArray[i]==' ')break;
-            index++;
-        }
-        return index;
-    }
-
-    private int findNumber(int StartIndex) {
-        int Minus = 1;
-        int Number =0;
-        for(int i=StartIndex;i<myArray.length;i++){
-            if(myArray[i]==' ')break;
-            if(myArray[i]=='-') {
-                Minus = -1;
-                continue;
-            }
-            Number = Number*10+Integer.valueOf(myArray[i]-'0');
-        }
-        return Minus*Number;
     }
 
 }
