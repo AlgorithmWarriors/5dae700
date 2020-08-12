b98ddd9a711f92ec7d01ce3aef23c78a9ba96fcf
ChUngHEe0227 <rogo0227@naver.com>
2020-08-13 08:57:45 +0900

    점프와 순간이동 수학적 풀이법

--- src/main/java/Chunghee/Level2/Teleporting.java
+++ src/main/java/Chunghee/Level2/Teleporting.java
@@ -2,13 +2,12 @@ package Chunghee.Level2;
 
 public class Teleporting {
     public int solution(int n) {
-        if(n==1||n==2)return 1;
-        int[] dp = new int [n+1];
-        dp[1]=dp[2]=1;
-        for(int i=3;i<=n;i++){
-            if(i/2*2==i)dp[i]=dp[i/2];
-            else dp[i]=dp[i/2]+1;
+        int answer = 1;
+
+        while (n!=1){
+            answer += n%2;
+            n/=2;
         }
-        return dp[n];
+        return answer;
     }
 }
